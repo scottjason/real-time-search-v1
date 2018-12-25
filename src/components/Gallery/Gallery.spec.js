4cf9c9db38815ec29e6a6137b019cfcc3add7267
Scott Jason <scottleejason@gmail.com>
2018-12-24 20:37:01 -0800

    finish gallery unit tests

--- src/components/Gallery/Gallery.spec.js
+++ src/components/Gallery/Gallery.spec.js
@@ -3,7 +3,6 @@ import { shallow, mount } from 'enzyme';
 import {fetchByTerm} from '../../api'
 import Gallery from './Gallery';
 import Search from '../Search/Search';
-import Image from '../Image/Image';
 
 /**
  *
@@ -22,32 +21,59 @@ describe('Gallery', () => {
 
   beforeEach(() => {
     wrapper = mount(<Gallery images={mockImages} />)
-    componentInstance = wrapper.childAt(0);
   });
 
   const createShallowWrapper = () => {
-    wrapper = shallow(<Gallery />)
-    componentInstance = wrapper.childAt(0);
+    wrapper = shallow(<Gallery images={mockImages} />)
   };
   
   /**
   * High-Level Checks
   */
+  test('it should exist', () => {
+    expect(wrapper).toExist(true);
+  });
+
   test('it should have correct name', () => {
     expect(wrapper.name()).toEqual('Gallery');
   });
 
   test('it should match snapshot', () => {
     expect(wrapper).toMatchSnapshot()
-  });  
+  });
+  
+  /**
+  * Props
+  */
+  test('it should have images defined', () => {
+    expect(wrapper.props().images).toBeDefined();
+  });
+  
+  test('it should have an images array', () => {
+    expect(wrapper.props().images).toBeInstanceOf(Array)
+  });
+  
+  test('it should have three images', () => {
+    expect(wrapper.props().images.length).toEqual(3)
+  });
 
   /**
-  * Element Existence Checks
+  * Search Component
   */
   test('it should should contain a <Search /> component', () => {
     expect(wrapper.contains(<Search />)).toEqual(true)
   });  
-  test('it should should contain an <Image /> component', () => {
-    expect(wrapper.contains(<Search />)).toEqual(true)
+  
+  test('it should should contain a form element', () => {
+    expect(wrapper.find('form')).toExist(true)
   });
+  
+  test('it should should contain an input element', () => {
+    expect(wrapper.find('input')).toExist(true)
+  });
+
+  test('search input should have right placeholder', () => {
+    expect(wrapper.find('input').props().placeholder).toEqual('Search images...')
+  });
+
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,5 @@
-import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-// jest.setTimeout(30000)
+import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
